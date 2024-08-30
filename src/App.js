1f27ffa979553ccd08a742e54a59e223851f739b
utyagijjwal <utyagi2003@gmail.com>
2024-08-31 03:13:50 +0530

    Education section added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import Navbar from './Components/Navbar'
 import HeroSection from './Components/HeroSection';
 import Ujjwal from './Components/Ujjwal';
+import Education from './Components/Education';
 import Footer from './Components/Footer';
+
 function App() {
   return (
     <div className="App">
     <Navbar/>
     <HeroSection/>
     <Ujjwal/>
+    <Education/>
     <Footer/>
     </div>
   );
